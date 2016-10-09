8c23a05efc1adbf82b17b5f0b09ab3bdcfe3eb65
Vivin Thomas Wilson <vivintw@yahoo-inc.com>
2016-10-09 12:41:54 +0530

    added usage

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ _usage :_
 ```
  python remove_watermark.py /path/to/file/file.pdf
 ```
+replaces the old file with the modified file
+```
+python remove_watermark.py /path/to/file/file.pdf /path/to/modified/file/file.py
+```
+makes a new file with the changes
+
--- remove_watermark.py
+++ remove_watermark.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import os.path
 
 '''
     Author: Vivin Thomas Wilson
@@ -8,12 +9,40 @@ import sys
     Description : Removes "Scanned by camscan" watermark from pdf 
 '''
 
-f = open(sys.argv[1])
-data = f.read().split("\n")
-f.close()
-f = open(sys.argv[1],"w")
-for i in data:
-    if not "(Scanned by CamScanner) Tj" in i:
-        f.write(i+"\n")
-f.close()
+def usage():
+    _WARN = '\033[91m'
+    _END = '\033[0m'
+    print ""
+    print 'USAGE :'
+    print _WARN+'python remove_watermark.py /path/to/file/file.pdf'+_END
+    print 'replaces the old file with the modified file'
+    print '(OR)'
+    print _WARN+'python remove_watermark.py /path/to/file/file.pdf /path/to/modified/file/file.py'+_END
+    print 'makes a new file with the changes'
+    print ""
+    exit(0)
+
+def wmark_remove(from_file,to_file):
+    f = open(from_file,'r')
+    data = f.read().split("\n")
+    f.close()
+
+    f = open(to_file,"w")
+    f.write("\n".join([i for i in data if not "(Scanned by CamScanner) Tj" in i]))
+    f.close()
+
+if __name__ == '__main__':
+
+    to_file = ""
+    from_file = ""
+
+    if len(sys.argv) == 2:
+       to_file = from_file = sys.argv[1]
+    elif len(sys.argv) == 3:
+       from_file = sys.argv[1]
+       to_file = sys.argv[2]
+    else:
+       usage()
+ 
+    wmark_remove(from_file,to_file)
 
