5f2aedcbd9b1750f23d1f1f4579b9709166d25e8
Vivin Thomas Wilson <vivintw@yahoo-inc.com>
2016-10-09 07:43:34 +0530

    added author info

--- remove_watermark.py
+++ remove_watermark.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 import sys
 
+'''
+    Author: Vivin Thomas Wilson
+    version : 1.0
+    python verson : Python 2.7.10 
+    Description : Removes "Scanned by camscan" watermark from pdf 
+'''
+
 f = open(sys.argv[1])
 data = f.read().split("\n")
 f.close()
