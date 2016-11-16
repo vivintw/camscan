e62896ec688b7864b3b9b56a9a46654ade96f333
Vivin Thomas Wilson <vivintw@yahoo-inc.com>
2016-11-16 16:18:27 +0530

    fixed length issues

--- remove_watermark.py
+++ remove_watermark.py
@@ -4,7 +4,7 @@ import os.path
 
 '''
     Author: Vivin Thomas Wilson
-    version : 1.1
+    version : 1.2
     python verson : Python 2.7.10 
     Description : Removes "Scanned by camscan" watermark from pdf 
 '''
@@ -22,25 +22,62 @@ def usage():
     print("")
     exit(0)
 
-def wmark_remove(from_file,to_file):
-    f = open(from_file,'r')
+
+def read_pdf_objects(input_file):
+    f = open(input_file)
     data = f.read().split("\n")
     f.close()
 
-    f = open(to_file,"w")
+    obj_store = {}
+    read = False
+    for i in data:
+       if " obj" in i:
+           read = True
+           name = int(i.split()[0])
+           tmp = ""
 
-    # remove the entire text object 
-    i = 0
-    while i < len(data):
-        if  "(Scanned by CamScanner) Tj" in data[i]:
-            while not "BT" in data[i]: 
-                i -= 1
-            while "ET" not in data[i]: 
-                del data[i]
-            del data[i]
-        i += 1
+       if read:
+           tmp += i+"\n"
 
-    f.write("\n".join(data))
+       if "endobj" in i:
+           read= False
+           obj_store[name] = tmp
+    return (obj_store,data)
+
+def remove_watermark(obj_store):
+    for key,value in obj_store.items():
+        if "Scanned by CamScanner" in value:
+            tmp_obj = value.split("\n")
+            obj_store[key] = ""
+            write = True
+            omitted = 0
+            for i in tmp_obj:
+              if "BT" in i :
+                  write = False
+              if write:
+                  obj_store[key] += i + "\n"
+              else:
+                 omitted += len(i) + 1
+              if "ET" in i:
+                  write = True
+            len_obj_key = int(value.split("<<")[1].split(">>")[0].strip().split()[1])
+            len_obj_val = obj_store[len_obj_key].split("\n")
+            len_obj_val[1] = str(int(len_obj_val[1]) - omitted)
+            obj_store[len_obj_key] =  "\n".join(len_obj_val)
+    return obj_store
+
+def write_file(data,obj_store,output_file):
+    f = open(output_file,"w")
+    write = True
+    for i in data:
+       if " obj" in i:
+           write = False
+           obj_key = int(i.split()[0])
+       if write:
+           f.write(i+"\n")
+       if "endobj" in i:
+           write = True
+           f.write(obj_store[obj_key])
     f.close()
 
 if __name__ == '__main__':
@@ -56,5 +93,6 @@ if __name__ == '__main__':
     else:
        usage()
  
-    wmark_remove(from_file,to_file)
-
+    obj_store,data = read_pdf_objects(from_file)
+    obj_store = remove_watermark(obj_store)
+    write_file(data,obj_store,to_file)
